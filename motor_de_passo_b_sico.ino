957065e3f357db76bd988f734cbc5f402f173d73
eglardo <68170474+eglardo@users.noreply.github.com>
2020-07-19 02:27:18 -0300

    Add files via upload

---
+++ motor_de_passo_b_sico.ino
@@ -0,0 +1,50 @@
+
+  // put your main code here,
+  
+int IN1 = 11;
+int IN2 = 10;
+int IN3 = 9;
+int IN4 = 8;
+void setup()
+{
+  // Define os pinos como saida
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+}
+
+int tempo = 8;
+
+void loop()
+{
+  // Passo 1
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+  delay(tempo);  
+
+  // Passo 2
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+  delay(tempo);
+
+  // Passo 3
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
+  delay(tempo);
+
+  // Passo 4
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
+  delay(tempo);
+}
+
+
